e3572b8c4c68189233a932ede8e5623c10e6897f
syedd49 <37467421+syedd49@users.noreply.github.com>
2019-09-27 15:26:45 +0530

    Create start-stopped-instances.py

---
+++ Python Boto3/start-stopped-instances.py
@@ -0,0 +1,17 @@
+# Start Stopped Instances
+
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+# Return type is ec2.Instance
+
+instances = ec2.instances.filter(Filters=[
+    {
+        "Name": "instance-state-name",
+        "Values": ['stopped']
+    }
+])
+
+for instance in instances:
+    instance.start()
